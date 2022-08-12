ceed3a9a4f4264eff3968a116a4d0e48cf67e725
Tianhao Wu <97497708+Tianhao-Wu-ABC@users.noreply.github.com>
2022-08-11 21:59:35 -0700

    Update webcam.py

--- flight test/webcam.py
+++ flight test/webcam.py
@@ -1,1 +1,13 @@
+import time
+import os
 
+num = 0
+path = os.getcwd()
+
+
+while True:
+	os.system('sudo fswebcam -r 1980x1080 --save %s/webcampics/img%s' % (path, str(num)))
+	num+=1
+	time.sleep(1)
+	if num == 10:
+		break
